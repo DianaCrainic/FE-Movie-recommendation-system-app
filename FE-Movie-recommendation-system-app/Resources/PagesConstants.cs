47b8939c4c8f5855257e9e4f25a9449df0d74390
Sabina Apostol <sabina.bubu@yahoo.com>
2020-12-21 20:48:45 +0200

    Fix ratings

--- FE-Movie-recommendation-system-app/Resources/PagesConstants.cs
+++ FE-Movie-recommendation-system-app/Resources/PagesConstants.cs
@@ -2,6 +2,7 @@
 {
     public const string LoginPage = "account/login";
     public const string HomePage = "/";
+    public const string MyRatings = "/ratings";
     public const string ActorsFromMoviePage = "movies";
     public const string AddRatingToMoviePage = "ratings/movie";
     public const string UpdateRatingPage = "ratings/update";
