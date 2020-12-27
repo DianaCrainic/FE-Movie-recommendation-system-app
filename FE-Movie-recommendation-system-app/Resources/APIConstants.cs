5bf3228f3ed91ea7a72ed094d9f59643b756e784
Ioan <ioan.sava28@gmail.com>
2020-12-27 19:49:01 +0200

    fix: Microservices

--- FE-Movie-recommendation-system-app/Resources/APIConstants.cs
+++ FE-Movie-recommendation-system-app/Resources/APIConstants.cs
@@ -3,12 +3,12 @@
     public const string BaseUrl = "http://localhost:5000";
     public const string JsonContent = "application/json";
     public const string AuthenticationHeaderValueBearer = "Bearer";
-    public const string AuthenticateEndpoint = "/api/v1/users/authenticate";
-    public const string UsersEndpoint = "/api/v1/users";
-    public const string RatingsEndpoint = "/api/v1/ratings";
-    public const string RatingsForUserEndpoint = "/api/v1/ratings/users";
-    public const string MoviesEndpoint = "/api/v1/movies";
-    public const string ActorsFromMovieEndpoint = "/api/v1/actors/movies";
-    public const string RecommendationsForUserEndpoint = "/api/v1/recommendations";
+    public const string AuthenticateEndpoint = "/users/authenticate";
+    public const string UsersEndpoint = "/users";
+    public const string RatingsEndpoint = "/ratings";
+    public const string RatingsForUserEndpoint = "/ratings/users";
+    public const string MoviesEndpoint = "/movies";
+    public const string ActorsFromMovieEndpoint = "/actors/movies";
+    public const string RecommendationsForUserEndpoint = "/recommendations";
     public const string NumberOfPagesHeader = "numberOfPages";
 }
