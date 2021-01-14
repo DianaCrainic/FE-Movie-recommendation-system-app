a5cc70c2b0cd5915e0076e9b65a616737873f51d
Bogdan <radumugur1997@gmail.con>
2021-01-14 22:44:23 +0200

    API POP-up

--- FE-Movie-recommendation-system-app/Shared/ImdbAPI.cs
+++ FE-Movie-recommendation-system-app/Shared/ImdbAPI.cs
@@ -1,173 +1,92 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
-using System.Threading.Tasks;
 
+public class Genre
+{
+    public int id { get; set; }
+    public string name { get; set; }
+}
 
+public class ProductionCompany
+{
+    public int id { get; set; }
+    public string logo_path { get; set; }
+    public string name { get; set; }
+    public string origin_country { get; set; }
+}
 
+public class ProductionCountry
+{
+    public string iso_3166_1 { get; set; }
+    public string name { get; set; }
+}
 
-    public class Image
+public class SpokenLanguage
+{
+    public string english_name { get; set; }
+    public string iso_639_1 { get; set; }
+    public string name { get; set; }
+}
+
+public class Root
+{
+    public bool adult { get; set; }
+    public string backdrop_path { get; set; }
+    public object belongs_to_collection { get; set; }
+    public int budget { get; set; }
+    public List<Genre> genres { get; set; }
+    public string homepage { get; set; }
+    public int id { get; set; }
+    public string imdb_id { get; set; }
+    public string original_language { get; set; }
+    public string original_title { get; set; }
+    public string overview { get; set; }
+    public double popularity { get; set; }
+    public string poster_path { get; set; }
+    public List<ProductionCompany> production_companies { get; set; }
+    public List<ProductionCountry> production_countries { get; set; }
+    public string release_date { get; set; }
+    public int revenue { get; set; }
+    public int runtime { get; set; }
+    public List<SpokenLanguage> spoken_languages { get; set; }
+    public string status { get; set; }
+    public string tagline { get; set; }
+    public string title { get; set; }
+    public bool video { get; set; }
+    public double vote_average { get; set; }
+    public int vote_count { get; set; }
+}
+
+public class ImdbAPI
+{
+    public string MovieDescription { get; set; }
+    public string PictureAddress { get; set; }
+    public string MovieTitle { set; get; }
+    public string MessageContent { set; get; }
+    public ImdbAPI()
     {
-        public int height { get; set; }
-        public string id { get; set; }
-        public string url { get; set; }
-        public int width { get; set; }
+
     }
 
-    public class Title
+    public async void GetMovieDetails(int movieId)
     {
-        public string Type { get; set; }
-        public string id { get; set; }
-        public Image image { get; set; }
-        public int runningTimeInMinutes { get; set; }
-        public string nextEpisode { get; set; }
-        public int numberOfEpisodes { get; set; }
-        public int seriesEndYear { get; set; }
-        public int seriesStartYear { get; set; }
-        public string title { get; set; }
-        public string titleType { get; set; }
-        public int year { get; set; }
-    }
-
-    public class US
-    {
-        public string certificate { get; set; }
-        public string country { get; set; }
-    }
-
-    public class Certificates
-    {
-        public List<US> US { get; set; }
-    }
-
-    public class OtherRank
-    {
-        public string id { get; set; }
-        public string label { get; set; }
-        public int rank { get; set; }
-        public string rankType { get; set; }
-    }
-
-    public class Ratings
-    {
-        public bool canRate { get; set; }
-        public double rating { get; set; }
-        public int ratingCount { get; set; }
-        public List<OtherRank> otherRanks { get; set; }
-    }
-
-    public class PlotOutline
-    {
-        public string author { get; set; }
-        public string id { get; set; }
-        public string text { get; set; }
-    }
-
-    public class PlotSummary
-    {
-        public string author { get; set; }
-        public string id { get; set; }
-        public string text { get; set; }
-    }
-
-    public class DetailsRoot
-    {
-        public string id { get; set; }
-        public Title title { get; set; }
-        public Certificates certificates { get; set; }
-        public Ratings ratings { get; set; }
-        public List<string> genres { get; set; }
-        public string releaseDate { get; set; }
-        public PlotOutline plotOutline { get; set; }
-        public PlotSummary plotSummary { get; set; }
-    }
-
-
-
-    public class Director
-    {
-        public string disambiguation { get; set; }
-        public string id { get; set; }
-        public Image image { get; set; }
-        public string legacyNameText { get; set; }
-        public string name { get; set; }
-        public string category { get; set; }
-        public int endYear { get; set; }
-        public int episodeCount { get; set; }
-        public int startYear { get; set; }
-        public List<string> akas { get; set; }
-    }
-
-    public class Writer
-    {
-        public string id { get; set; }
-        public Image image { get; set; }
-        public string legacyNameText { get; set; }
-        public string name { get; set; }
-        public List<string> attr { get; set; }
-        public string category { get; set; }
-        public int endYear { get; set; }
-        public int episodeCount { get; set; }
-        public string job { get; set; }
-        public int startYear { get; set; }
-        public List<string> akas { get; set; }
-        public string disambiguation { get; set; }
-    }
-
-    public class ActorsRoot
-    {
-        public List<Director> directors { get; set; }
-        public List<Writer> writers { get; set; }
-    }
-
-
-   
-    public class ImdbAPI
-    {
-        public  string MovieDescritpion { get; set; }
-        public  string PictureAddress { get; set; }
-        public  string MovieTitle { set; get; }
-        public string MessageContent { set; get; } 
-        public ImdbAPI()
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
         {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://api.themoviedb.org/3/movie/{movieId}?api_key=bbaf97a3069fad0118f94c81a1d7cc9e")
+        };
 
-        }
-
-
-        public async void GetMovieDetails(int movieId)
-        {
-
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://imdb8.p.rapidapi.com/title/get-overview-details?tconst=tt0{movieId}&currentCountry=US"),
-                Headers =
-                    {
-                    { "x-rapidapi-key","b3ba47dd7amshf8f0f527cfbd1f8p16fa06jsnf385995af860" }, /**/ 
-                    { "x-rapidapi-host", "imdb8.p.rapidapi.com" },
-                },
-            };
-            
-            using (var response = await client.SendAsync(request))
-            {
-                try
-                {
-                    response.EnsureSuccessStatusCode();
-
-                }catch(Exception e)
-                {   
-                    return;
-                }
-                var body = await response.Content.ReadAsStringAsync();
-                DetailsRoot details = JsonSerializer.Deserialize<DetailsRoot>(body, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
-                MovieDescritpion = details.plotSummary.text;
-                MovieTitle = details.title.title;
-                PictureAddress = details.title.image.url;
-            }
-        }
-    }   
+        Root details;
+        using var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        var body = await response.Content.ReadAsStringAsync();
+        details = JsonSerializer.Deserialize<Root>(body, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        MovieDescription = details.overview;
+        MovieTitle = details.title;
+        PictureAddress = "https://image.tmdb.org/t/p/original" + details.poster_path;
+    }
+}
 
